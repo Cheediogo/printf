a8ca1643f5b3d8cb553ee0688704ca9fb7bdb0af
christian <christianudoh84@gmail.com>
2022-07-13 16:52:53 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
+ * check_for_specifiers -> checks if there is a valid format specifier
  * @format: possible format specifier
- *
  * Return: pointer to valid function or NULL
  */
 static int (*check_for_specifiers(const char *format))(va_list)
@@ -38,9 +37,8 @@ static int (*check_for_specifiers(const char *format))(va_list)
 }
 
 /**
- * _printf - prints anything
+ * _printf -> prints anything
  * @format: list of argument types passed to the function
- *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
